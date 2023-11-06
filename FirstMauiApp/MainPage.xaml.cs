db4b649c60c9bfbc84e95709e0c481c3a2cb3fa1
Chase Hutcheson <111781666+ChaseHutcheson@users.noreply.github.com>
2023-11-06 13:04:31 -0500

    Add project files.

---
+++ FirstMauiApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace FirstMauiApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+		{
+			CounterBtn.Text = $"Clicked {count} time";
+			label.Text = "Hey, you clicked it once";
+		}
+		else
+		{
+			CounterBtn.Text = $"Clicked {count} times";
+			label.Text = "Hey, you clicked it more than once";
+		}
+        SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
